96b77637c5ef05aae43ef84864d48bd93da71a56
Joaquin Abregu <abregujoaquin@gmail.com>
2021-10-23 18:00:51 -0300

    initial commit

---
+++ src/pages/Login.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "../components/Form";
+
+export default function Login() {
+  return (
+    <Container fluid="sm">
+      <Row className="justify-content-center">
+        <Col xs={3}>
+          <Form />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
